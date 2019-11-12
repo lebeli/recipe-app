ce9f03944fdf30fc6addb15072cd78d90900619a
Lie Leonhard Benjamin <ll040@hdm-stuttgart.de>
2019-11-12 20:12:30 +0100

    Feat/backend init

---
+++ backend/fluffy-bear/src/test/java/com/hdmstuttgart/fluffybear/FluffyBearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdmstuttgart.fluffybear;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluffyBearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
