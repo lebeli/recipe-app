c5b5d2f57410c41b5dafe7ad9bcf477a598c0236
Manuela Müller <mm243@hdm-stuttgart.de>
2019-11-18 14:31:41 +0100

    devServer

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack")
 
 module.exports = {
   entry: "./src/index.jsx",
@@ -22,11 +23,11 @@ module.exports = {
     publicPath: "/dist/",
     filename: "bundle.js"
   },
-  // devServer: {
-  //   contentBase: path.join(__dirname, "public/"),
-  //   port: 3000,
-  //   publicPath: "http://localhost:3000/dist/",
-  //   hotOnly: true
-  // },
-  //plugins: [new webpack.HotModuleReplacementPlugin()]
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
 };
