ea217a75589602accb45f8ce577a3b9f5a573898
Erik Großkopf <erik@grosskopfonline.de>
2019-12-31 03:49:23 +0100

    Add demo images and controller for handling image requests

--- backend/src/main/java/com/hdmstuttgart/fluffybear/FluffyBearApplication.java
+++ backend/src/main/java/com/hdmstuttgart/fluffybear/FluffyBearApplication.java
@@ -1,13 +1,26 @@
 package com.hdmstuttgart.fluffybear;
 
+import com.hdmstuttgart.fluffybear.Storage.StorageProperties;
+import com.hdmstuttgart.fluffybear.Storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class FluffyBearApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FluffyBearApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
