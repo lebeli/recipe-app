7c843055f1a195e8fcf30889243c690e631ba628
Johanna Reiting <johanna.reiting@googlemail.com>
2020-01-13 11:36:18 +0100

    added clock icon to header

--- frontend/src/RecommenderHeader.jsx
+++ frontend/src/RecommenderHeader.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import "./Recommender.scss";
 
 function RecommenderHeader(params) {
   return (
     <div className="Header">
       <div className="HeaderContent">{params.recipeName}</div>
-      <div className="HeaderContent">{params.time}</div>
+      <div className="HeaderContent">
+        <AccessTimeIcon />
+        {params.time}
+      </div>
     </div>
   );
 }
