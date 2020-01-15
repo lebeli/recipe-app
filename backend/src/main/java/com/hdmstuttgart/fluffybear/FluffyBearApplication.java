bd038ae24e09c0d848b21802d556dc9c18722f9b
Leonhard Lie <ll040@hdm-stuttgart.de>
2020-01-15 12:37:19 +0100

    Add documentation for FluffyBearApplication

--- backend/src/main/java/com/hdmstuttgart/fluffybear/FluffyBearApplication.java
+++ backend/src/main/java/com/hdmstuttgart/fluffybear/FluffyBearApplication.java
@@ -6,13 +6,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
+/**
+ * Class for initiating the application context (retrival of models, services and repositories).
+ */
 @SpringBootApplication
 public class FluffyBearApplication {
 
+	/**
+	 * Entrypoint for Fluffy-Bear-Application.
+	 *
+	 * @param args  spring beans for IoC container.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(FluffyBearApplication.class, args);
 	}
 
+
+	/**
+	 * Inititiates repositories at a cleant state.
+	 *
+	 * @param storageService  FileSystemStorageService instance.
+	 * @return  spring bean for repository initialization at application startup.
+	 */
 	@Bean
 	CommandLineRunner init(StorageService storageService) {
 		return (args) -> {
